6a3c81058e7971e0b3a8a828a6743814f75833a8
Milan Vukosavljev <vukosavljevmilan2@gmail.com>
2022-12-04 11:30:29 +0100

    Write down pseude code

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello World!");
+// User should enter a word(rock, paper, sissors) via prompt
+// The user input will be stored in a varable and compared with
+// a computers random generated one
+// Game should have a count for the rounds
+// First one to win 3 rounds is a winner
