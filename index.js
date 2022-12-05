4b0146e356135d87b3505d333cddee381bb7cf12
Milan Vukosavljev <vukosavljevmilan2@gmail.com>
2022-12-05 10:00:39 +0100

    add player an computer choice with the computer picking a random choice

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 // a computers random generated one
 // Game should have a count for the rounds
 // First one to win 3 rounds is a winner
+ 
+// let userChoice = prompt("What is your choice?: ");
+let choice = prompt("What do you choose?: ");
+function playerChoice() {
+    return choice.toLocaleLowerCase();
+}
+let player = playerChoice();
+console.log(player);
+const choices= ['rock','paper','sissors'];
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+let computer = getComputerChoice();
+
+
